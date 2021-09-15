2e25b888648d7ed2a38ba193716a2f4f811ad2a4
satocchi0416sh <satoyoshi44@gmail.com>
2021-09-16 03:00:18 +0900

    add fixed buttons and title

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
 import { useState } from "react"
 import ApplicationHome from './component/application';
 function App() {
-  const [currentPage, setCurrentPage] = useState("top")
 
   let page;
-  if (currentPage === "top") {
-    page = <div className="all-wrapper">
-      <h1>ようこそテストアプリへ</h1>
-      <button onClick={() => { setCurrentPage("explanation") }}>次に進む</button>
-    </div>
-  } else if (currentPage === "explanation") {
-    page = <div className="all-wrapper">
-      <h1>説明</h1>
-      <h2>このアプリはこうこうこういうアプリです</h2>
-      <button onClick={() => { setCurrentPage("app") }}>さっそく始める</button>
-    </div>
-  } else {
-    page = <ApplicationHome />
-  }
+
+  page = <ApplicationHome />
 
   return (
     <div className="App">
